ea5963020d1b86a7de385151f83f6f1c49e9913b
Earl Lee <earlvlee@gmail.com>
2012-10-08 21:34:20 -0400

    added menu feature

--- wp-content/themes/yaledailynews/functions.php
+++ wp-content/themes/yaledailynews/functions.php
@@ -141,7 +141,7 @@ function ydn_scripts() {
 	global $post;
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_style( 'bootstrap-ydn', get_template_directory_uri() . '/css/ydn.css');
+  wp_enqueue_style( 'bootstrap-ydn', get_stylesheet_uri() . '/css/ydn.css');
 
   wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap/bootstrap.js', array('jquery') );
   wp_enqueue_script( 'throttle-debounce', get_template_directory_uri() . '/js/jquery.ba-throttle-debounce.min.js', array('jquery') );
