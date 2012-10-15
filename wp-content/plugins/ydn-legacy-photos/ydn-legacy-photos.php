5876c7c58c05d2c7876dc7d5311948f813e5ab86
Earl Lee <earlvlee@gmail.com>
2012-10-15 00:44:49 -0400

    fixed parenth

--- wp-content/plugins/ydn-legacy-photos/ydn-legacy-photos.php
+++ wp-content/plugins/ydn-legacy-photos/ydn-legacy-photos.php
@@ -15,11 +15,11 @@ function ydn_legacy_photos_filter($atts) {
 	$id = $atts[id];
 
 	if ( wp_attachment_is_image( $id ) ) {
-		$src = wp_get_attachment_image($id, medium);
+		#$src = wp_get_attachment_image($id, medium);
 		print "ID variable is $id";
 		#var_dump($src);
 		#var_dump($atts);
-		$return_string = '<div class="inline inline-left">' . '<img src="' . $src[0] . '" />' . get_media_credit_html($id) . ' </div>';
+		$return_string = '<div class="inline inline-left">' . wp_get_attachment_image($id, medium) . get_media_credit_html($id) . ' </div>';
 		return $return_string;
 	}
 
