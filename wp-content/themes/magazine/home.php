ffcaeac1799aefc637ddd3b5ed62acc742069074
Michael DiScala <mike.discala@gmail.com>
2012-10-27 16:34:54 -0400

    removed broken custom field

--- wp-content/themes/magazine/home.php
+++ wp-content/themes/magazine/home.php
@@ -4,7 +4,6 @@
  *  Description: all this has to do is grab the latest issue of the magazine from the database,
  *  setup the $post variable appropriately, and then include the issue template
  * */
-
 $latest_issue_query = array("post_type" => YDN_Mag_Issue_Type::type_slug,
                             "post_status" => "publish",
                             "posts_per_page" => 1);
