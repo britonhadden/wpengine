474c54d63088ae0e1f66129795fe6a8fa2374115
Earl Lee <earlvlee@gmail.com>
2012-11-11 22:37:06 -0500

    made image.php fix for crosscampus

--- wp-content/themes/crosscampus/image.php
+++ wp-content/themes/crosscampus/image.php
@@ -55,8 +55,7 @@ get_header();
 								?>
 
 								<a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
-									$attachment_size = apply_filters( 'ydnxc_attachment_size', array( 1200, 1200 ) ); // Filterable image size.
-									echo wp_get_attachment_image( $post->ID, $attachment_size );
+									echo wp_get_attachment_image( $post->ID, 'entry-featured-image' );
 								?></a>
 							</div><!-- .attachment -->
 
