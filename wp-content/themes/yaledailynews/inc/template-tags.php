05b515a16015d573902ff32cb28974d5af2bb1c0
Michael DiScala <michael.discala@gmail.com>
2012-11-25 16:19:47 -0500

    fixed comment count in main theme

--- wp-content/themes/yaledailynews/inc/template-tags.php
+++ wp-content/themes/yaledailynews/inc/template-tags.php
@@ -365,7 +365,7 @@ if (! function_exists("ydn_comment_count") ):
     global $post;
     ?>
     <a href="<?php echo get_permalink();?>#comments-title" class="comment-count">
-      <span class="dsq-postid" rel="<?php htmlspecialchars(dsq_identifier_for_post($post))?>">(<?php echo $post->comment_count; ?>)</span>
+      <span class="dsq-postid" rel="<?php echo htmlspecialchars(dsq_identifier_for_post($post)); ?>"><?php echo $post->comment_count; ?></span>
     </a>
     <?php
   }
