4b9c5e9fb358bb95a572fa00f9ae40a36dce0563
Michael DiScala <michael.discala@gmail.com>
2012-11-25 16:33:39 -0500

    fixed minify config

--- wp-content/plugins/bwp-minify/min/config.php
+++ wp-content/plugins/bwp-minify/min/config.php
@@ -49,7 +49,7 @@ $min_enableBuilder = false;
 //$min_cachePath = 'c:\\WINDOWS\\Temp';
 //$min_cachePath = '/tmp';
 //$min_cachePath = preg_replace('/^\\d+;/', '', session_save_path());
-$min_cachePath = dirname(WP_CONTENT_DIR . '/minify-cache/';
+$min_cachePath = dirname(WP_CONTENT_DIR . '/minify-cache/');
 
 
 /**
