8fd4c9bb67966b1067e4c78db294722433f93c28
Earl Lee <earlvlee@gmail.com>
2012-11-27 22:02:55 -0500

    archives page added

--- wp-content/themes/yaledailynews/archive-page.php
+++ wp-content/themes/yaledailynews/archive-page.php
@@ -5,22 +5,24 @@ Template Name: Archives
 get_header(); ?>
 
 <div id="primary" class="site-content">
-	<div id="content" role="main">
+	<div id="content" class="archive" role="main">
 
 		<?php the_post(); ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 		
 		<?php get_search_form(); ?>
-		
-		<h2>Archives by Month:</h2>
+	
+		<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+  <option value=""><?php echo esc_attr( __( 'Select Day' ) ); ?></option> 
+  <?php wp_get_archives( 'type=daily&format=option&show_post_count=1' ); ?>
+</select>
+	
+		<h2>Archives by Year:</h2>
 		<ul>
-			<?php wp_get_archives('type=monthly'); ?>
+			<?php wp_get_archives('type=yearly'); ?>
 		</ul>
 		
-		<h2>Archives by Subject:</h2>
-		<ul>
-			 <?php wp_list_categories(); ?>
-		</ul>
+		<?php the_content(); ?>		
 
 	</div><!-- #content -->
 </div><!-- #container -->
