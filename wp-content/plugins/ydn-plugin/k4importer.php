07c1786aed758df3a6dccc97e87aeb2a48e3401d
Akshay Nathan <akshaynathan@client-24.ydn.yale.edu>
2013-01-14 19:57:42 -0500

    test k4 importer custom plugin

---
+++ wp-content/plugins/ydn-plugin/k4importer.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: K4Importer
+Description: Importer for K4.
+Version: 1.0
+Author: Akshay Nathan
+Author URI: http://URI_Of_The_Plugin_Author
+License: GPL2
+*/
+	echo("HELLO WORLD");
+
+	class k4Importer {
+		function import_story($xml_string) {
+			$xml = new SimpleXMLElement($xml_string);
+			// Extract Title
+			$title = $xml->headline->hl1->trim();
+			// Extract Authors into array with primary author first
+				// Split on and? get rid of whitespace and //'s
+			// Extract story html
+			$story = $xml->{'body.content'}->trim();
+			// Extract abstract, or leave blank if theres nothing but space
+			$excerpt = $xml->abstract->trim();
+			// Get the category/ies
+		}
+	}
+?>
