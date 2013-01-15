056bc622892f93ea9b6c8fe9bab0262afdd40458
Akshay Nathan <akshaynathan@client-24.ydn.yale.edu>
2013-01-14 20:58:04 -0500

    test k4 importer custom plugin

--- wp-content/plugins/ydn-plugin/k4importer.php
+++ wp-content/plugins/ydn-plugin/k4importer.php
@@ -13,8 +13,8 @@ License: GPL2
 	add_action( 'init', 'add_importer_endpoint' );
 	function importer_template_redirect() {
 		global $wp_query;
-		echo("THIS IS A TEST");
-		print_r( $wp_query->query_vars );
+		echo("THIS IS A TEST\n");
+		print_r($_POST);	
 		exit;
 	}
 	add_action( 'template_redirect', 'importer_template_redirect' );
