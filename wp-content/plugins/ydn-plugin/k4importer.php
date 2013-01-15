733d699d1e3f07dbdf204f5607b2021850cb0a0e
Akshay Nathan <akshaynathan@client-24.ydn.yale.edu>
2013-01-14 21:30:49 -0500

    hopefully fixed plugin

---
+++ wp-content/plugins/ydn-plugin/k4importer.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: K4Importer
+Description: Importer for K4.
+Version: 1.0
+Author: Akshay Nathan
+Author URI: http://URI_Of_The_Plugin_Author
+License: GPL2
+*/
+	function add_importer_endpoint() {
+		add_rewrite_endpoint( 'importer', EP_ROOT );
+	}
+	add_action( 'init', 'add_importer_endpoint' );
+	function importer_template_redirect() {
+		global $wp_query;
+		if ( ! isset( $wp_query->query_vars['importer'] ) || ! is_singular() )
+                	return;
+		echo("Importer! Post requests to this page will be imported.\n");
+		if(!empty($_POST)) {
+			wp_mail("akshay.nathan08@gmail.com", "POST REQUEST", implode(",", $_POST));	
+			echo(implode(",", $_POST));
+		}
+		exit;
+	}
+	add_action( 'template_redirect', 'importer_template_redirect' );
+
+	function makeplugins_endpoints_activate() {
+        	makeplugins_endpoints_add_endpoint();
+        	flush_rewrite_rules();
+	}
+	register_activation_hook( __FILE__, 'makeplugins_endpoints_activate' );
+ 
+	function makeplugins_endpoints_deactivate() {
+		flush_rewrite_rules();
+	}
+	register_deactivation_hook( __FILE__, 'makeplugins_endpoints_deactivate' );
+?>
