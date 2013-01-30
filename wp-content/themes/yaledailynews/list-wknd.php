53bf2a8770fbabd72b1711847d83234455014abe
earllee <earlvlee@gmail.com>
2013-01-29 22:11:00 -0500

    removed timestamp and excerpt from wknd widget

--- wp-content/themes/yaledailynews/list-wknd.php
+++ wp-content/themes/yaledailynews/list-wknd.php
@@ -1,6 +1,4 @@
 <div class="item">
   <?php $cat = ydn_get_top_level_cat(); ?>
   <a class="headline" href="<?php echo get_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-  <div class="datetime"><?php echo ydn_timestamp(); ?></div>
-  <div class="teaser"><?php echo get_post_meta($post->ID,'ydn_homepage_excerpt',true); ?> <a href="<?php echo get_permalink(); ?>">››</a></div>
 </div>
