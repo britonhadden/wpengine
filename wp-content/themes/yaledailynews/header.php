59d7e8705d8c4c2381f9660781be7cff01ca4e7b
earllee <earlvlee@gmail.com>
2013-01-30 22:48:30 -0500

    added gAnalytics tracking

--- wp-content/themes/yaledailynews/header.php
+++ wp-content/themes/yaledailynews/header.php
@@ -42,6 +42,20 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+<!-- Google Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38103759-1']);
+  _gaq.push(['_trackPageview']);
+
+    (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+  </script>
 <!-- ComScore Analytics Script -->
 <script>
   var _comscore = _comscore || [];
