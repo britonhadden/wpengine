80365dd632cac765bf12cc925008e319ae600379
Akshay Nathan <akshaynathan@client-21.ydn.yale.edu>
2013-02-04 20:03:06 -0500

    debug for k4

--- wp-content/plugins/ydn-plugin/k4importer.php
+++ wp-content/plugins/ydn-plugin/k4importer.php
@@ -20,40 +20,6 @@
                 	return;
 		echo("Importer! An endpoint to import k4 into the wordpress dbs.\n");
         wp_mail("akshay.nathan08@gmail.com", "REQUEST!", implode(",", $_GET));
-
-/*
-		if( isset($_GET['NITFurl']) ) {
-            $url = $_GET['NITFurl'];
-echo("$url\n");
-$xml = file_get_contents("http://130.132.127.186:8080/K4XML/k4_nitf_1109_w10_column3_je%20[P]_8603326.xml");
-
-/*
- $xml_object = new SimpleXMLElement($xml);
-            
-            $author = $xml_object->body->{'body.head'}->byline->person;
-            $title = $xml_object->body->{'body.head'}->byline->byttl;
-            $excerpt = $xml_object->body->{'body.head'}->abstract;
-            $story =  $xml_object->body->{'body.content'};
-            
-echo("$author $title");
-            $first_name = explode(" ", $author);
-            $last_name = $first_name[1];
-            $first_name = $first_name[0];
-            $query = $wpdb->prepare("SELECT user_id  FROM $wpdb->usermeta WHERE ( meta_key='first_name' AND meta_value='%s' ) and ( meta_key='last_name' AND meta_value='%s' )", $first_name ,$last_name);
-            $authorID= $wpdb->get_var( $query );
-		
-	echo("$first_name $last_name $authorID $title $excerpt $story");
-*//*
-            $post = array(
-              'post_author'    => 'test',
-              'post_content'   => $xml,
-              'post_excerpt'   => 'test',
-              'post_name'      => 'test',
-              'post_status'    => 'draft',
-            );  
-            wp_insert_post( $post );
-*/
-        }
 		exit;
 	}
 	add_action( 'template_redirect', 'importer_template_redirect' );
