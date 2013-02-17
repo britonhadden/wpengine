94eaf7f4a6033cd8a74ef15cd4cd30b3de0aaf2f
earllee <earlvlee@gmail.com>
2013-02-17 18:47:57 -0500

    multimedia structured page.

--- wp-content/themes/yaledailynews/multimedia.php
+++ wp-content/themes/yaledailynews/multimedia.php
@@ -4,15 +4,23 @@ Template Name: Multimedia
 */
 get_header(); ?>
 
-<div id="primary" class="site-content">
-	<div id="content" class="archive" role="main">
-	<?php the_post();?>		
+	<div class="span24">
+		<div class="row">
+			<div id="main-theater" class="span24">weekly broadcast</div>
+		</div>
+		<div class="row">
+			<div id="slider" class="span24">individual packages</div>
+		</div>
+		<div class="row">
+			<div class="span12 archives-box">archives one</div>
+			<div class="span12 archives-box">archives two</div>
+		</div>
+	</div>
 
-		
+		<?php the_post();?>		
 	
 		<?php the_content(); ?>		
 
-	</div><!-- #content -->
 </div><!-- #container -->
 
 <?php get_footer(); ?>
