66675e9bf94fbd1cf6b6e122011878daae0b0e64
Akshay Nathan <akshay.nathan@yale.edu>
2013-02-28 16:02:33 -0500

    importer stuff

--- wp-content/plugins/ydn-plugin/k4importer.php
+++ wp-content/plugins/ydn-plugin/k4importer.php
@@ -19,8 +19,8 @@
 		if ( ! isset( $wp_query->query_vars['importer'] ) )
                 	return;
 		echo("Importer! An endpoint to import k4 into the wordpress dbs.\n");
-        if( ! empty($_POST)  )
-            echo("Post request received.");
+        if( ! empty($_POST) || ! empty($_GET))
+            echo("Request received.");
         wp_mail("akshay.nathan08@gmail.com", "REQUEST!", "GET:" . implode(",", $_GET) . "POST:" . implode(",", $_POST));
 		exit;
 	}
