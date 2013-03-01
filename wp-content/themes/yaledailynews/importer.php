0f972555bd85243d5e422fe24e51aa7b22d5641d
Akshay Nathan <akshay.nathan@yale.edu>
2013-02-28 19:48:19 -0500

    importer?

--- wp-content/themes/yaledailynews/importer.php
+++ wp-content/themes/yaledailynews/importer.php
@@ -3,11 +3,9 @@
 Template Name: Importer
 */
 ?>
-<?php get_header(); ?>
 HELLO
 <?php
     wp_mail("akshay.nathan08@gmail.com", "REQUEST!", "GET:" . implode(",", $_GET) . "POST:" . implode(",", $_POST));
 ?>
 
-<?php get_footer(); ?>
 
