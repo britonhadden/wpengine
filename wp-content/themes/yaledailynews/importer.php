c150efa778842f63a22f198c67fa8ecf70fba1db
Akshay Nathan <akshay.nathan@yale.edu>
2013-02-28 19:53:14 -0500

    importer?

--- wp-content/themes/yaledailynews/importer.php
+++ wp-content/themes/yaledailynews/importer.php
@@ -5,11 +5,10 @@ Template Name: Importer
 ?>
 <?php
 echo("Importer! An endpoint to import k4 into the wordpress dbs.\n");
-if( ! empty($_POST) || ! empty($_GET))
+if( ! empty($_POST) || ! empty($_GET)) {
     echo("Request received.");
     wp_mail("akshay.nathan08@gmail.com", "REQUEST!", "GET:" . implode(",", $_GET) . "POST:" . implode(",", $_POST));
 }
-
 ?>
 
 
