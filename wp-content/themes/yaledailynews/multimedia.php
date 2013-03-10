087a12c83bc8e70bdc458305a09ff30fc90d89c6
earllee <earlvlee@gmail.com>
2013-03-09 23:09:26 -0500

    multimedia slider mostly styled. theatre styled. header area styled.

--- wp-content/themes/yaledailynews/multimedia.php
+++ wp-content/themes/yaledailynews/multimedia.php
@@ -13,10 +13,10 @@ get_header(); ?>
 		</div>
 		<div class="row">
             <div id="slider" class="span24">
-
+<ul>
 <?php
     global $post;
-    $args = array( 'numberposts' => 5, 'post_type' => 'video', 'category' => 3034 ); // 3034 is the category id for a package, we should really have numberposts = infinity and get all the posts and only display a certain amount at a time. 
+    $args = array( 'numberposts' => 7, 'post_type' => 'video', 'category' => 3034 ); // 3034 is the category id for a package, we should really have numberposts = infinity and get all the posts and only display a certain amount at a time. 
     $myposts = get_posts( $args );
     foreach( $myposts as $post ) :
         setup_postdata($post);
@@ -25,13 +25,15 @@ get_header(); ?>
             $video_id = $match[1];
         }
 ?>
-    <a class="thumbnail">
-        <img src="http://img.youtube.com/vi/<?php echo $video_id;?>/default.jpg"/>
-        <?php the_title(); ?>
+<li>
+	<a href="#" rel="tooltip" class="thumbnail-video"  title="<?php the_title(); ?>">
+        <img class="thumbnail-youtube" src="http://img.youtube.com/vi/<?php echo $video_id;?>/0.jpg"/>
     </a>
+</li>
 <?php
     endforeach;
 ?>
+</ul>
             </div>
 		</div>
 		<div class="row">
@@ -43,8 +45,13 @@ get_header(); ?>
 		<?php the_post();?>		
 	
 		<?php the_content(); ?>		
+ <script src="http://twitter.github.com/bootstrap/assets/js/bootstrap-tooltip.js"></script>
+				<script type="text/javascript">
+				$(document).ready(function() { 
+					$('.thumbnail-video').tooltip();
+				});
 
+				</script>
 </div><!-- #container -->
 
 <?php get_footer(); ?>
-
