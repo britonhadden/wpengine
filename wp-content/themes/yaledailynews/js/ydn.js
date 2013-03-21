c463b66b0ee1e4f3e4a1c7a90a58c97ac5f58166
Akshay Nathan <akshay.nathan@yale.edu>
2013-03-21 16:42:44 -0700

    ajax

--- wp-content/themes/yaledailynews/js/ydn.js
+++ wp-content/themes/yaledailynews/js/ydn.js
@@ -68,7 +68,7 @@
         // Put the first post into the player
         var first = posts[0];
         console.log("Loading First Video");
-        $("#main-theater").load(
+        $("#main-theater").html(
 "<iframe id=\"video-player\" src=\"http://youtube.com/embed/" + first.vid_id + "frameborder=0></iframe>"
                 );
         
