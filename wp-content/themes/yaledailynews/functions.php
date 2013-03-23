1ddcf92e790344427c8c8d694c9ac952463f0f50
Akshay Nathan <akshay.nathan@yale.edu>
2013-03-22 20:45:08 -0700

    ...

--- wp-content/themes/yaledailynews/functions.php
+++ wp-content/themes/yaledailynews/functions.php
@@ -259,6 +259,19 @@ function ydn_attachment_fields_to_edit($form_fields, $post) {
 }
 add_filter("attachment_fields_to_edit", "ydn_attachment_fields_to_edit", 100, 2);
 
+// fix custom post types (video)
+add_filter('pre_get_posts', 'query_post_type');
+function query_post_type($query) {
+  if(is_category() || is_tag() || is_home() && empty( $query->query_vars['suppress_filters'] ) ) {
+    $post_type = get_query_var('post_type');
+	if($post_type)
+	    $post_type = $post_type;
+	else
+	    $post_type = array('post','articles','nav_menu_item');
+    $query->set('post_type',$post_type);
+	return $query;
+    }
+}
 
 /**
  * A handler to save the attachment fields when the edit form
