1aa4326dfdad235540546ecbd95c2aa8a6a128da
earllee <earlvlee@gmail.com>
2013-04-12 13:25:06 -0400

    added home page leaderboard header code

--- wp-content/themes/yaledailynews/header.php
+++ wp-content/themes/yaledailynews/header.php
@@ -100,6 +100,30 @@ googletag.enableServices();
 });
 </script>
 
+<!-- Homepage leaderboard -->
+<script type='text/javascript'>
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+(function() {
+var gads = document.createElement('script');
+gads.async = true;
+gads.type = 'text/javascript';
+var useSSL = 'https:' == document.location.protocol;
+gads.src = (useSSL ? 'https:' : 'http:') + 
+'//www.googletagservices.com/tag/js/gpt.js';
+var node = document.getElementsByTagName('script')[0];
+node.parentNode.insertBefore(gads, node);
+})();
+</script>
+
+<script type='text/javascript'>
+googletag.cmd.push(function() {
+googletag.defineSlot('/1041068/YDN_Home_Leaderboard', [728, 90], 'div-gpt-ad-1365622060397-0').addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.enableServices();
+});
+</script>
+
 <?php wp_head(); ?>
 </head>
 
