93cd341e2a6515e93496fcc99d9e3f4496dc94df
Akshay Nathan <akshay.nathan@yale.edu>
2013-04-21 22:08:41 -0400

    changing importer

--- wp-content/themes/yaledailynews/importer.php
+++ wp-content/themes/yaledailynews/importer.php
@@ -6,7 +6,7 @@ Template Name: Importer
 <?php
 global $wpdb;
 echo("Importer! An endpoint to import k4 into the wordpress dbs.\n");
-$url = $_GET['NITFURL'];
+$url = $_GET['NITFurl'];
 if($url) {
     $xml = file_get_contents($url);
     echo $xml;
