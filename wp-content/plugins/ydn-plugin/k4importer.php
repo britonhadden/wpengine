d41a3b3f3f721568f80d653b4425cecef8210296
Vincent Hu <vincent.hu@yale.edu>
2014-01-27 23:36:47 -0500

    Changed email for k4 test

--- wp-content/plugins/ydn-plugin/k4importer.php
+++ wp-content/plugins/ydn-plugin/k4importer.php
@@ -21,7 +21,7 @@
 		echo("Importer! An endpoint to import k4 into the wordpress dbs.\n");
         if( ! empty($_POST) || ! empty($_GET))
             echo("Request received.");
-        wp_mail("akshay.nathan08@gmail.com", "REQUEST!", "GET:" . implode(",", $_GET) . "POST:" . implode(",", $_POST));
+        wp_mail("ydnimportarchive@gmail.com", "REQUEST!", "GET:" . implode(",", $_GET) . "POST:" . implode(",", $_POST));
 		exit;
 	}
 	add_action( 'template_redirect', 'importer_template_redirect' );
